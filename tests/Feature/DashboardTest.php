76c78750f6358e0e9b85531937f56127feb3e4c5
Mansour Abdullah <manodullah@gmail.com>
2026-06-25 14:49:24 +0300

    fix tests

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -10,16 +10,26 @@ class DashboardTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_guests_are_redirected_to_the_login_page()
+    public function test_guests_are_redirected_to_the_login_page(): void
     {
         $response = $this->get(route('admin.dashboard'));
         $response->assertRedirect(route('login'));
     }
 
-    public function test_authenticated_users_can_visit_the_dashboard()
+    public function test_non_admin_users_are_redirected_to_the_homepage(): void
     {
         /** @var User $user */
         $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this->get(route('admin.dashboard'));
+        $response->assertRedirect(route('home'));
+    }
+
+    public function test_admin_users_can_visit_the_dashboard(): void
+    {
+        /** @var User $user */
+        $user = User::factory()->admin()->create();
         $this->actingAs($user);
 
         $response = $this->get(route('admin.dashboard'));
