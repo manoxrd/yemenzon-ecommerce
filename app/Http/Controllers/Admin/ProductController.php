20485a2ec39a3a574499de62a76bd1cc221160cd
Mansour Abdullah <manodullah@gmail.com>
2026-06-29 00:07:25 +0300

    splitted the ProductController to tow parts, admin and client sides

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ProductController extends Controller
+{
+  public function Index()
+  {
+    $products = Product::all();
+
+    return Inertia::render('admin/products/Index', [
+      'products' => $products,
+    ]);
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    //
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Product $product) {}
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, Product $product)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Product $product)
+  {
+    //
+  }
+}
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,60 +19,11 @@ class ProductController extends Controller
             'products' => $products,
         ]);
     }
-
-    public function adminIndex()
-    {
-        $products = Product::all();
-
-        return Inertia::render('admin/products/Index', [
-            'products' => $products,
-        ]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
+    
     public function show(Product $product)
     {
         return Inertia::render('products/Show', [
             'product' => $product,
         ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Product $product) {}
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Product $product)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Product $product)
-    {
-        //
     }
 }
