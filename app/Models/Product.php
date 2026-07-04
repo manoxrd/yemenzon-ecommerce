36eedad37b0c4bdca3bd4628bea0b4a893726c0a
Mansour Abdullah <manodullah@gmail.com>
2026-07-04 12:00:21 +0300

    created realationship between Product and Category models and migrations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,15 +5,21 @@ namespace App\Models;
 use Database\Factories\ProductFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     /** @use HasFactory<ProductFactory> */
     use HasFactory;
 
-    public function user()
+    public function user(): BelongsTo
     {
 
         return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo {
+
+      return $this->belongsTo(Category::class);
     }
 }
